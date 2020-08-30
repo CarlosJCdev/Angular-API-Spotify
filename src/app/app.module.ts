11b06ca6a30d18047db6b008c9a3043d394e948d
CarlosJCdev <carlos4561059919@gmail.com>
2020-08-30 01:48:24 -0500

    se agrego un unico componente para trabajar con las tarjetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { SpotifyService } from './services/spotify.service';
 import { HttpClientModule } from '@angular/common/http';
 import { NoimagePipe } from './pipes/noimage.pipe';
+import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { NoimagePipe } from './pipes/noimage.pipe';
     SearchComponent,
     ArtistaComponent,
     NavbarComponent,
-    NoimagePipe
+    NoimagePipe,
+    TarjetasComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pipes/noimage.pipe.ts
+++ src/app/pipes/noimage.pipe.ts
@@ -8,11 +8,11 @@ export class NoimagePipe implements PipeTransform {
   transform(images: any[]): string {
     //Validamos si al buscar una imagen, no trae imagen de portada osea si el valor es null o undefined
     if( !images){
-      return 'assets/banner-ico.png';
+      return 'assets/img/banner-ico.png';
     } if( images.length >0){
       return images[0].url;
     } else{
-      return 'assets/banner-ico.png';
+      return 'assets/img/banner-ico.png';
     }
   }
 
