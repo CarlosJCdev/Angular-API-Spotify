6136a091fd50f23776f1fc344922ef94564c5876
CarlosJCdev <carlos4561059919@gmail.com>
2020-08-31 23:20:56 -0500

    Se agrego una tabla debajo de la foto del artista en artista component, y se consume el método getTopTracks, para mostrar la data

--- src/app/components/artista/artista.component.ts
+++ src/app/components/artista/artista.component.ts
@@ -11,20 +11,30 @@ export class ArtistaComponent {
 
   artista: any= {};
   loading: boolean;
+  topTracks: any[]= [];
 
   constructor(private routerA: ActivatedRoute, private service: SpotifyService) {
     this.loading= true;
     this.routerA.params.subscribe( params =>{
       this.getArtista(params ['id']);
+      this.getTopTracks( params ['id']);
     })
    }
 
    getArtista(id: string){
     this.loading= true;
     this.service.getArtista(id).subscribe(artista =>{
-      console.log(artista);
+     /*  console.log(artista); */
       this.artista= artista;
       this.loading= false;
     });
    }
+
+   getTopTracks(id: string){
+    this.service.getTopTracks( id ).subscribe( topTracks =>{
+      console.log(topTracks);
+      this.topTracks= topTracks;
+    })
+   }
+ 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -16,18 +16,16 @@ export class SpotifyService {
    getQuery(query: string){
      const url= `https://api.spotify.com/v1/${ query }`;
      const headers= new HttpHeaders({
-      'Authorization': 'Bearer BQDWpeKG0Emd71wK0Vj6Lgvn-rB6aM6P-wk5B28J-Sg9KZr1WYRg8UV4owX7DwM2i5sdVWw846nMxNb4nZI'
+      'Authorization': 'Bearer BQBEsdGsFcfO4IPrcD9UFMX-mKLKqJQRwlzRc4mZqIEECGBkQ07agmfUvacxBjsNyESU-UQhhXdGXQGSsgE'
     });
     return this.http.get(url, {headers});
    }
    
-
   getNewMusic(){
  return this.getQuery('browse/new-releases')
   .pipe(map(data => data['albums'].items ));
   //Podemos colocar el typo de dato (data:string), o colocar ['albums'] de esta manera.
 }
-
 
 getArtistas(termino: string){
   return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
@@ -38,6 +36,12 @@ getArtistas(termino: string){
 getArtista(id: string){
   return this.getQuery(`artists/${ id }`)
  /* .pipe(map(data=> data['artists'].items )); */
+  //Puedo simplificar esto ya que es de una sola linea
+} 
+
+getTopTracks(id: string){
+  return this.getQuery(`artists/${ id }/top-tracks?country=us`)
+  .pipe(map(data=> data['tracks'] ));
   //Puedo simplificar esto ya que es de una sola linea
 } 
 
